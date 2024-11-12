a8b04cbd0f60a54283052c5114f87b534813afd3
RishabhSk7 <2002tej@gmail.com>
2024-11-13 00:23:50 +0530

    added precedece according to DMAS

--- Main.cpp
+++ Main.cpp
@@ -145,8 +145,10 @@ private:
     }
 
     int getPrecedence(const std::string& op) {
-        if (op == "+" || op == "-") return 1;
-        if (op == "*" || op == "/") return 2;
+        if ( op == "-") return 1;
+        if (op == "+") return 2;
+        if (op == "*" ) return 3;
+        if (op == "/") return 4;
         return 0;
     }
 
